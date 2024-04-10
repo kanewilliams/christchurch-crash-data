7af52291649418ae12b193a0635ae478c6e0f38f
Kane <5062932+kanewilliams@users.noreply.github.com>
2024-04-10 18:32:18 +1200

    Mage to BigQuery
    Now the Mage Pipeline uploads to BigQuery, in addition Google Cloud Storage

---
+++ magic-zoomcamp/data_exporters/crash_to_bq.py
@@ -0,0 +1,32 @@
+from mage_ai.settings.repo import get_repo_path
+from mage_ai.io.bigquery import BigQuery
+from mage_ai.io.config import ConfigFileLoader
+from pandas import DataFrame
+from os import path
+
+if 'data_exporter' not in globals():
+    from mage_ai.data_preparation.decorators import data_exporter
+
+
+@data_exporter
+def export_data_to_big_query(df: DataFrame, **kwargs) -> None:
+    """
+    Template for exporting data to a BigQuery warehouse.
+    Specify your configuration settings in 'io_config.yaml'.
+
+    Docs: https://docs.mage.ai/design/data-loading#bigquery
+    """
+
+    project_id = 'christchurch-crash-data'
+    dataset_name = 'raw_traffic_data'
+    table_name = 'crash_data'
+
+    table_id = f"{project_id}.{dataset_name}.{table_name}"
+    config_path = path.join(get_repo_path(), 'io_config.yaml')
+    config_profile = 'default'
+
+    BigQuery.with_config(ConfigFileLoader(config_path, config_profile)).export(
+        df,
+        table_id,
+        if_exists='replace',  # Specify resolution policy if table name already exists
+    )
